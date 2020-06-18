58f4716a0a43f9969ca21e4d5460b6739e487402
Aadil Desai <aadil4991@gmail.com>
2020-06-17 23:05:55 -0400

    Handle Errors on API and Angular

---
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { error } from 'protractor';
+
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+      return next.handle(req).pipe(
+          catchError(err => {
+              if (err.status === 401){
+                return throwError(err.statusText);
+              }
+              if (err instanceof HttpErrorResponse) {
+                  const applicationError = err.headers.get('Application-Error');
+                  if (applicationError) {
+                      return throwError(applicationError);
+                  }
+                  const serverError = err.error;
+                  let modelStateErrors = '';
+                  if (serverError.errors && typeof serverError.errors === 'object') {
+                      for (const key in serverError.errors) {
+                          if (serverError.errors[key]) {
+                              modelStateErrors += serverError.errors[key] + '\n';
+                          }
+                      }
+                  }
+                  return throwError(modelStateErrors || serverError || 'Server Error');
+              }
+          })
+      );
+    }
+
+}
+
+export const ErrorInterceptorProvider = {
+    provide : HTTP_INTERCEPTORS,
+    useClass : ErrorInterceptor,
+    multi : true
+}
