39c0a089efa842688620627424c6c40786da7fcb
Aadil Desai <aadil4991@gmail.com>
2020-06-20 12:29:34 -0400

    Extended USerClass, Learnt about Migrations and Relationships, Seeding Data in Db, Automapper

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.DataTransferObjects;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>().ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+            .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailDto>().ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+              .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));;
+            CreateMap<Photo, PhotosForDetailsDto>();
+        }
+    }
+}
